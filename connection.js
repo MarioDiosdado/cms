0c2d9f555dae271f6996ae5eade0fb004e5e7cf2
Mario Diosdado <diosdado.mario@gmail.com>
2020-10-27 10:54:03 -0500

    Fixed pass

--- connection.js
+++ connection.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "Laparrilla82",
+    password: "Yourpassword",
     database: "cmsDB"
 });
 //This is the initialization function
